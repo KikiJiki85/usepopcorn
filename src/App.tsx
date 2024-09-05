dac62eb05e5a703d57591848d4f8986b1db68754
Aleksey Vaskin <alzor85@gmail.com>
2024-09-05 15:18:58 +0300

    refactor: make a reusable box component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { tempMovieData } from './assets/mock';
+import { tempMovieData, tempWatchedData } from './assets/mock';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 import NumResults from './components/NumResults';
 import Logo from './components/Logo';
 import Search from './components/Search';
-import ListBox from './components/ListBox';
-import WatchedBox from './components/WatchedBox';
+import Box from './components/Box';
 import MovieList from './components/MovieList';
+import WatchedSummary from './components/WatchedSummary';
+import WatchedMovieList from './components/WatchedMovieList';
 
 function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   return (
     <>
       <NavBar>
@@ -19,10 +21,15 @@ function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+        <Box>
+          <>
+            <WatchedSummary watched={watched} />
+            <WatchedMovieList watched={watched} />
+          </>
+        </Box>
       </Main>
     </>
   );
---
+++ src/components/Box.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, useState } from 'react';
+
+interface BoxProps {
+  children: ReactNode;
+}
+
+function Box({ children }: BoxProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? '–' : '+'}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+export default Box;
