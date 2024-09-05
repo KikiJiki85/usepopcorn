9720ad048305b8d34e3f8e921304ab8d36d65cf0
Aleksey Vaskin <alzor85@gmail.com>
2024-09-05 07:22:42 +0300

    refactor: split app into small components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
-import { tempMovieData, tempWatchedData } from './assets/mock';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 
 function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} watched={watched} />
+      <NavBar />
+      <Main />
     </>
   );
 }
---
+++ src/components/NumResults.tsx
@@ -0,0 +1,9 @@
+function NumResults() {
+  return (
+    <p className="num-results">
+      Found <strong>{}</strong> results
+    </p>
+  );
+}
+
+export default NumResults;
