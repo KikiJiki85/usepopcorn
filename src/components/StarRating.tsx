7babd30c1a719756ed2d8cf0e82bd222feeb491c
Aleksey Vaskin <alzor85@gmail.com>
2024-09-07 06:00:26 +0300

    refactor: StarRating component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -29,8 +29,8 @@ function StarRating({
   defaultRating = 0,
   onSetRating,
 }: StarRatingProps) {
-  const [rating, setRating] = useState(defaultRating);
-  const [tempRating, setTempRating] = useState(0);
+  const [rating, setRating] = useState<number>(defaultRating);
+  const [tempRating, setTempRating] = useState<number>(0);
 
   const textStyle = {
     lineHeight: '1',
@@ -60,9 +60,9 @@ function StarRating({
         ))}
       </div>
       <p style={textStyle}>
-        {messages.length !== maxRating
-          ? tempRating || rating || ''
-          : messages[tempRating ? tempRating - 1 : rating - 1]}
+        {messages.length && messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
       </p>
     </div>
   );
@@ -88,7 +88,7 @@ function Star({
   const starStyle = {
     display: 'flex',
     width: `${size}px`,
-    heigh: `${size}px`,
+    height: `${size}px`,
     cursor: 'pointer',
   };
 
@@ -119,7 +119,7 @@ function Star({
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
+            strokeWidth={1}
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
